7485d92500b582f201a3e361aa361851fc667395
kw-8 <kwu11235813@gmail.com>
2021-08-17 08:08:19 -0700

    set up methods done

--- katherine_wu_hangman_project/lib/hangman.rb
+++ katherine_wu_hangman_project/lib/hangman.rb
@@ -1,4 +1,17 @@
 class Hangman
   DICTIONARY = ["cat", "dog", "bootcamp", "pizza"]
+  
+  attr_reader :guess_word, :attempted_chars, :remaining_incorrect_guesses
+
+  def self.random_word
+    DICTIONARY.sample
+  end
+
+  def initialize
+    @secret_word = self.class.random_word
+    @guess_word = Array.new(@secret_word.length){"_"}
+    @attempted_chars = []
+    @remaining_incorrect_guesses = 5
+  end
 
 end
